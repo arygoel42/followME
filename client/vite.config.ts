9eebdaaf63a52fc90a1747a87e603da786f978ed
Aryan Goel <aryan_goel@outlook.com>
2024-08-26 07:03:01 -0400

    mabye

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: '../client/dist' // Adjust if necessary
+    outDir: 'dist' // Adjust if necessary
   },
   plugins: [react()]
 });
