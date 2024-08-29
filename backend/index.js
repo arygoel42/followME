8de3a8cf4a516446ad79dc54e530477cc97bda6e
Aryan Goel <aryan_goel@outlook.com>
2024-08-29 17:30:09 -0400

    added header to the redirect

--- backend/index.js
+++ backend/index.js
@@ -44,24 +44,15 @@ const sslOptions = {
 };
 
 app.get('/api/auth/instagram', async (req, res) => {
-   
-        try {
-            const response = await axios.get('https://api.instagram.com/oauth/authorize', {
-                params: {
-                    client_id: '8852423798110118',
-                    redirect_uri: 'https://follow-me-nbqo-7iyt678o3-arygoel42s-projects.vercel.app/api/callback',
-                    scope: 'user_profile,user_media',
-                    response_type: 'code'
-                }
-            });
-            res.json(response.data);
-        } catch (error) {
-            res.status(500).send('Error fetching data from Instagram API');
-        }
+    // const params_1 = new URLSearchParams();
+    // params_1.append('client_id', clientId);
+    // params_1.append('redirect_uri', hardcodedRedirectURI);
+    // params_1.append('scope', 'user_profile,user_media');
+    // params_1.append('response_type', 'code');
     
 
-    // res.redirect(`https://api.instagram.com/oauth/authorize?client_id=8852423798110118&redirect_uri=https://follow-me-nbqo-7iyt678o3-arygoel42s-projects.vercel.app/api/callback&scope=user_profile,user_media&response_type=code`);
-    // console.log('authenticating user')
+    res.redirect(`https://api.instagram.com/oauth/authorize?client_id=8852423798110118&redirect_uri=https://follow-me-nbqo-7iyt678o3-arygoel42s-projects.vercel.app/api/callback&scope=user_profile,user_media&response_type=code`, {headers: {'Access-Control-Allow-Origin': '*'}, headers: {'Cross-Origin-Resource-Policy': 'cross-origin'}});
+    console.log('authenticating user')
 
    
     
