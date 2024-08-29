b024ef2c5a3dcd7baf763a8c6d9f4f1059bf4b90
Aryan Goel <aryan_goel@outlook.com>
2024-08-29 10:58:58 -0400

    middlewear

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,15 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://your-frontend-domain.com'); // Replace with your frontend URL
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
