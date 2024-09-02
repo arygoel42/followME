a820396726d19488480682799b020bf874f167ce
Aryan Goel <aryan_goel@outlook.com>
2024-09-02 12:14:25 -0400

    fE

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import router from "./components/router.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <RouterProvider router={router} />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
