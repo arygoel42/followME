05c2f6746a83a661c08fd09af3cd6283721d9d74
Aryan Goel <aryan_goel@outlook.com>
2024-09-03 15:37:41 -0400

    added to vecel

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  root: './client', // Adjust this to point to the correct root directory
-  build: {
-    outDir: 'dist', // Ensure this points to the correct output directory
-  },
+   // Adjust this to point to the correct root directory
+  
 });
